52cff82131752e57296394050688568863c799fe
cn-wx <xuwang2@student.unimelb.edu.au>
2019-09-22 02:36:23 +1000

    gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
 var del = require('del');
+// var rename = require('gulp-rename');
+
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
+// var less = require('gulp-less');
+// var cleanCSS = require('gulp-clean-css');
+
 var paths = {
+//   styles: {
+//     src: 'src/styles/**/*.less',
+//     dest: 'dist/styles/'
+//   },
   scripts: {
     src: 'src/*.js',
     dest: 'dist/scripts/'
@@ -14,6 +23,19 @@ var paths = {
 function clean() {
     return del([ 'dist' ]);
 }
+
+// function styles() {
+//     return gulp.src(paths.styles.src)
+//       .pipe(less())
+//       .pipe(cleanCSS())
+//       // pass in options to the stream
+//       .pipe(rename({
+//         basename: 'main',
+//         suffix: '.min'
+//       }))
+//       .pipe(gulp.dest(paths.styles.dest));
+//   }
+  
 
 function scripts() {
     return gulp.src(paths.scripts.src, { sourcemaps: true })
@@ -25,11 +47,13 @@ function scripts() {
 
 function watch() {
     gulp.watch(paths.scripts.src, scripts);
+    // gulp.watch(paths.styles.src, styles);
 }
 
 var build = gulp.series(clean, gulp.parallel(scripts));
 
 exports.clean = clean;
+// exports.styles = styles;
 exports.scripts = scripts;
 exports.watch = watch;
 exports.build = build;
